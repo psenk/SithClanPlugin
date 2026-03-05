973befb5d22490fba7fb6ccf96f411dc5d34ce42
Tyler <psenk168@gmail.com>
2026-03-05 15:47:32 -0600

    added api key for info posting, notification enabling, notification timing

--- src/main/java/com/sithclanplugin/SithClanPluginConfig.java
+++ src/main/java/com/sithclanplugin/SithClanPluginConfig.java
@@ -4,16 +4,39 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
+// TODO: reshuffle config items on panel (currently alphabetically per name value)
+
 @ConfigGroup("sithclanplugin")
 public interface SithClanPluginConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "senateapikey",
+		name = "Senate API Key",
+		description = "API Key for posting clan info to server"
 	)
-	default String greeting()
+	default String apiKey()
 	{
-		return "Hello";
+		return "";
 	}
+
+	@ConfigItem(
+		keyName = "eventAlerts",
+		name = "Event Alerts",
+		description = "Enables or disables notifications for upcoming clan events"
+	)
+	default boolean eventNotifications()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = "eventAlertTimeBuffer",
+		name = "Alert Time Buffer",
+		description = "Sets amount of time before start of event notification occurs"
+	)
+	default int notificationTimeBuffer()
+	{
+		return 15;
+	}
+
 }
