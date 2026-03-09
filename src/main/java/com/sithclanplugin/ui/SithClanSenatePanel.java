292a3f37a7e8b81d29c87c0c255d372dabbec526
Tyler <psenk168@gmail.com>
2026-03-09 15:47:10 -0500

    refactored code created panel for loading schedule

--- src/main/java/com/sithclanplugin/ui/SithClanSenatePanel.java
+++ src/main/java/com/sithclanplugin/ui/SithClanSenatePanel.java
@@ -4,52 +4,115 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.sithclanplugin.eventschedule.SithClanEventSchedule;
 
+import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.util.ImageUtil;
+
 @Singleton
 public class SithClanSenatePanel extends JPanel {
 
     private final JLabel senatePanelLabel;
+    private final JLabel senatePostScheduleLabel;
+    private final JPanel senatePostSchedulePanel;
+    private final JScrollPane senatePostScheduleScrollPane;
+    private final JTextArea senatePostScheduleTextArea;
     private final JButton senatePostScheduleButton;
 
     private SithClanEventSchedule eventSchedule;
 
     private static final String SENATE_OPTIONS_LABEL = "Senate Options";
-    private static final String UPLOAD_SCHEDULE_BUTTON = "Post Schedule";
-    private static final String testData = "[{\"test\":\"data\"}]";
+    private static final String UPDATE_SCHEDULE_BUTTON = "Update Schedule";
+    private static final String UPDATE_BUTTON = "Update";
+    private static final String TEXT_AREA_DEFAULT = "Post Event Schedule Here";
+    private static final String ARROW_RIGHT_PATH = "../resources/arrow_right.png";
+    private static final String ARROW_DOWN_PATH = "../resources/arrow_down.png";
 
     @Inject
     SithClanSenatePanel(SithClanEventSchedule eventSchedule) {
-
+        
         this.eventSchedule = eventSchedule;
+        final Icon rightArrowIcon = new ImageIcon(ImageUtil.loadImageResource(getClass(), ARROW_RIGHT_PATH));
+        final Icon downArrowIcon = new ImageIcon(ImageUtil.loadImageResource(getClass(), ARROW_DOWN_PATH));
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        // main panel label at top
         senatePanelLabel = new JLabel(SENATE_OPTIONS_LABEL);
         senatePanelLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        senatePostScheduleButton = new JButton(UPLOAD_SCHEDULE_BUTTON);
-        senatePostScheduleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-
         this.add(senatePanelLabel);
         this.add(Box.createRigidArea(new Dimension(0, 10)));
-        this.add(senatePostScheduleButton);
-        this.setVisible(true);
+
+        // label to collapse/expand schedule panel
+        senatePostScheduleLabel = new JLabel(UPDATE_SCHEDULE_BUTTON);
+        senatePostScheduleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        senatePostScheduleLabel.setOpaque(true);
+        senatePostScheduleLabel.setIcon(rightArrowIcon);
+        senatePostScheduleLabel.setBackground(ColorScheme.DARKER_GRAY_COLOR);
+        senatePostScheduleLabel.setMaximumSize(new Dimension(Integer.MAX_VALUE, senatePostScheduleLabel.getPreferredSize().height));
+        this.add(senatePostScheduleLabel);
+
+        // panel to post the event schedule
+        senatePostSchedulePanel = new JPanel();
+        senatePostSchedulePanel.setLayout(new BoxLayout(senatePostSchedulePanel, BoxLayout.Y_AXIS));
+        senatePostScheduleTextArea = new JTextArea();
+
+        //TODO: change below lines
+        senatePostScheduleTextArea.setText(TEXT_AREA_DEFAULT);
+
+        senatePostScheduleTextArea.setRows(20);
+        senatePostScheduleTextArea.setLineWrap(false);
+        senatePostScheduleScrollPane = new JScrollPane(senatePostScheduleTextArea);
+        senatePostScheduleScrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
+        senatePostScheduleButton = new JButton(UPDATE_BUTTON);
+        senatePostScheduleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        // adding to main panel
+        senatePostSchedulePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        senatePostSchedulePanel.add(senatePostScheduleScrollPane);
+        senatePostSchedulePanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        senatePostSchedulePanel.add(senatePostScheduleButton);
+        senatePostSchedulePanel.setVisible(false);
+        this.add(senatePostSchedulePanel);
+
+        //expand/collapse event schedule options
+        senatePostScheduleLabel.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                boolean isVisible = !senatePostSchedulePanel.isVisible();
+                senatePostSchedulePanel.setVisible(isVisible);
+                if (isVisible) {
+                    senatePostScheduleLabel.setIcon(downArrowIcon);
+                }
+                else {
+                    senatePostScheduleLabel.setIcon(rightArrowIcon);
+                }
+                revalidate();
+                repaint();
+            }
+        });
 
         // post event schedule action
         senatePostScheduleButton.addActionListener((new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-                eventSchedule.postEventSchedule(testData);
+                //TODO: validate data (what if they input something else?)
+                String schedule = eventSchedule.parseScheduleForPost(senatePostScheduleTextArea.getText());
+                // if response good, popup good?
             }
         }));
     }
-
 }
