eda0cab6288e6b1886463db8f3fda3ff932758c9
Tyler <psenk168@gmail.com>
2026-03-19 17:01:47 -0500

    created class for file management and supporting functions for individual event subscription

--- src/main/java/sithclanplugin/SithClanPluginFileManager.java
+++ src/main/java/sithclanplugin/SithClanPluginFileManager.java
@@ -8,31 +8,49 @@ import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import net.runelite.client.RuneLite;
-import sithclanplugin.eventschedule.SithClanDaySchedule;
-
-// TODO: save event subscriptions
-// TODO: load event subscriptions
-// SithClanEventSchedule object instead of ArrayList<SithClanDaySchedule>?
 
 @Singleton
 public class SithClanPluginFileManager {
 
-    @Inject
-    private SithClanNotificationManager notificationManager;
-
-    private ArrayList<SithClanDaySchedule> eventSchedule;
     private final File localDirectory;
     private final File storedScheduleFile;
+    private final File storedSubscriptionsFile;
 
-    @Inject
-    public SithClanPluginFileManager(ArrayList<SithClanDaySchedule> eventSchedule) {
-        this.eventSchedule = eventSchedule;
+    public SithClanPluginFileManager() {
         this.localDirectory = new File(RuneLite.RUNELITE_DIR, SithClanPluginConstants.LOCAL_DIRECTORY_NAME);
         this.storedScheduleFile = new File(localDirectory, SithClanPluginConstants.STORED_SCHEDULE_NAME);
+        this.storedSubscriptionsFile = new File(localDirectory, SithClanPluginConstants.STORED_SUBSCRIPTIONS_NAME);
+    }
+
+    /**
+     * Creates plugin specific files and directories
+     * 
+     * @throws Exception
+     */
+    public void initializeFiles() throws Exception {
+        // create plugin folder if does not exist
+        if (!localDirectory.exists())
+            localDirectory.mkdirs();
+
+        // create saved schedule file if does not exist
+        if (!storedScheduleFile.exists())
+            storedScheduleFile.createNewFile();
+
+        // create file that saves event notification subscriptions
+        if (!storedSubscriptionsFile.exists())
+            storedSubscriptionsFile.createNewFile();
+    }
+
+    /**
+     * Checks if user has a saved schedule for loading
+     * 
+     * @return boolean if saved schedule exists
+     */
+    public boolean hasSavedSchedule() {
+        return storedScheduleFile.length() > 0;
     }
 
     /**
@@ -51,22 +69,85 @@ public class SithClanPluginFileManager {
     /**
      * Gets event schedule from local file for display on panel
      * 
-     * @return int SithClanPluginConstants status code value
+     * @return String json schedule as string
      */
-    public int parseScheduleFromFile() {
+    public String readScheduleFile() {
         try {
             String jsonSchedule = new String(Files.readAllBytes(storedScheduleFile.toPath()));
-            if (jsonSchedule.isBlank())
-                return SithClanPluginConstants.STATUS_BAD_INPUT;
-            Gson gson = new Gson();
-            Type scheduleType = new TypeToken<ArrayList<SithClanDaySchedule>>() {
-            }.getType();
-            this.eventSchedule = gson.fromJson(jsonSchedule, scheduleType);
-            notificationManager.scheduleNotifications(eventSchedule);
-            return SithClanPluginConstants.STATUS_OK;
+            return jsonSchedule.isBlank() ? null : jsonSchedule;
         } catch (Exception e) {
             e.printStackTrace();
-            return SithClanPluginConstants.STATUS_NOT_FOUND;
+            return null;
         }
+    }
+
+    /**
+     * Saves list of event subscriptions
+     * 
+     * @param subscriptions ArrayList<String> list of event subscriptions to save
+     */
+    private void saveSubscriptions(ArrayList<String> subscriptions) {
+        try (FileWriter fileWriter = new FileWriter(storedSubscriptionsFile)) {
+            fileWriter.write(new Gson().toJson(subscriptions));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Obtains the list of saved event subscriptions
+     * 
+     * @return ArrayList<String> list of subscribed events
+     */
+    private ArrayList<String> loadSubscriptions() {
+        try {
+            String input = new String(Files.readAllBytes(storedSubscriptionsFile.toPath()));
+            if (input.isBlank())
+                return new ArrayList<>();
+            Gson gson = new Gson();
+            Type listType = new TypeToken<ArrayList<String>>() {
+            }.getType();
+            ArrayList<String> subscriptions = gson.fromJson(input, listType);
+
+            return subscriptions != null ? subscriptions : new ArrayList<>();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    /**
+     * Add new event subscription
+     * 
+     * @param eventTitle String name of event
+     */
+    public void addSubscription(String eventTitle) {
+        ArrayList<String> subscriptions = loadSubscriptions();
+        if (!subscriptions.contains(eventTitle)) {
+            subscriptions.add(eventTitle);
+            saveSubscriptions(subscriptions);
+        }
+    }
+
+    /**
+     * Remove subscription to event
+     * 
+     * @param eventTitle String name of event
+     */
+    public void removeSubscription(String eventTitle) {
+        ArrayList<String> subscriptions = loadSubscriptions();
+        subscriptions.remove(eventTitle);
+        saveSubscriptions(subscriptions);
+    }
+
+    /**
+     * Finds out if user is subscribed to event or not
+     * 
+     * @param eventTitle String name of event
+     * @return boolean whether event is subscribed or not
+     */
+    public boolean isSubscribed(String eventTitle) {
+        ArrayList<String> subscriptions = loadSubscriptions();
+        return subscriptions.contains(eventTitle);
     }
 }
