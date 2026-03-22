1215b460b39c209f114ad67f2e0a3d0c8f3aa8f0
Tyler <psenk168@gmail.com>
2026-03-22 15:45:55 -0500

    filled out all remaining function functionality

--- src/main/java/sithclanplugin/members/SithClanMemberRoster.java
+++ src/main/java/sithclanplugin/members/SithClanMemberRoster.java
@@ -1,11 +1,20 @@
 package sithclanplugin.members;
 
+import java.net.http.HttpClient;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Collection;
 import java.util.HashMap;
 
+import com.google.gson.Gson;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import lombok.Getter;
+import sithclanplugin.SithClanPluginConfig;
+import sithclanplugin.util.SithClanPluginConstants;
+import sithclanplugin.util.SithClanPluginUtil;
 
 /**
  * Member Roster Object
@@ -15,57 +24,152 @@ import lombok.Getter;
 @Singleton
 public class SithClanMemberRoster {
 
+    @Inject
+    private SithClanPluginConfig config;
+
+    @Inject
+    private HttpClient httpClient;
+
     private HashMap<String, SithClanMember> roster;
     private ZonedDateTime dateRosterPosted;
 
     public SithClanMemberRoster() {
         roster = new HashMap<>();
-        dateRosterPosted = ZonedDateTime.now();
+        dateRosterPosted = null;
     }
 
     /**
-     * TODO: JAVADOC
-     * TODO: FUNCTIONALITY
+     * Creates and sends an HTTP GET request to obtain the event roster
      * 
-     * @return
+     * @return String member roster in an HTTP response body
      */
     private String getMemberRoster() {
-        return "";
+        return SithClanPluginUtil.sendGetRequest(httpClient, SithClanPluginConstants.MEMBER_ROSTER_GET_URI);
     }
 
     /**
-     * TODO: JAVADOC
-     * TODO: FUNCTIONALITY
+     * Creates and sends HTTP POST request to post new member roster
      * 
-     * @return
+     * @param jsonData String JSON member roster in string format
+     * @return String HTTP Response body with status code
      */
-    private String postRoster(String jsonData) {
-        return "";
+    private String postMemberRoster(String jsonData) {
+        return SithClanPluginUtil.sendPostRequest(httpClient, config.apiKey(), jsonData,
+                SithClanPluginConstants.MEMBER_ROSTER_POST_URI);
     }
 
     /**
-     * TODO: JAVADOC
-     * TODO: FUNCTIONALITY
+     * Gets member roster
      * 
-     * @return
+     * TODO: rate limiting?
+     * 
+     * @return int SithClanPluginConstants status code value
      */
     public int parseRosterFromGet() {
-        return 0;
+        // get fresh member roster
+        String jsonRoster = getMemberRoster();
+        if (jsonRoster == null) {
+            return SithClanPluginConstants.STATUS_NOT_FOUND;
+        }
+        // convert roster to JSON
+        this.roster = deserializeRoster(jsonRoster);
+        return SithClanPluginConstants.STATUS_OK;
     }
 
     /**
-     * TODO: JAVADOC
-     * TODO: FUNCTIONALITY
+     * Takes String input and converts to JSON format for posting
      * 
-     * @return
+     * @param rosterInput String member roster from plugin text box
+     * @return int SithClanPluginConstants status code value
      */
     public int parseRosterForPost(String rosterInput) {
-        return 0;
+        if (rosterInput.isBlank()) {
+            return SithClanPluginConstants.STATUS_BAD_INPUT;
+        }
+
+        // split input into list of strings
+        String[] rosterInputList = rosterInput.split("\\r?\\n");
+        // turn list into member roster
+        HashMap<String, SithClanMember> newRoster = convertRoster(rosterInputList);
+        if (newRoster == null || newRoster.isEmpty())
+            return SithClanPluginConstants.STATUS_BAD_INPUT;
+
+        // convert roster to array for worker
+        Collection<SithClanMember> rosterOutput = newRoster.values();
+
+        // store roster as JSON object
+        Gson gson = new Gson();
+        String data = gson.toJson(rosterOutput);
+
+        // post roster
+        String response = postMemberRoster(data);
+        if (response == null) {
+            return SithClanPluginConstants.STATUS_NOT_FOUND;
+        }
+        // save roster
+        this.roster = newRoster;
+        return SithClanPluginConstants.STATUS_OK;
+    }
+
+    /**
+     * Converts member roster String list into custom object list
+     * 
+     * @param rosterInput member roster in String[] list
+     * @return HashMap<String, SithClanMember> converted member roster
+     */
+    private HashMap<String, SithClanMember> convertRoster(String[] rosterInput) {
+
+        HashMap<String, SithClanMember> newRoster = new HashMap<>();
+
+        // iterate through roster list
+
+        // name,userRank,credits,discordUser,dateJoined,altName,datePromoted
+        // 10hp Vibe,2,27,182971719944896512,10/23/23,,03/21/24
+        for (String member : rosterInput) {
+            member = member.trim();
+            if (member.isBlank() || member.startsWith("name,")) {
+                continue;
+            }
+            // parse member info
+            String[] memberInfo = member.split(",");
+            SithClanMember newMember = new SithClanMember();
+            newMember.setMemberName(memberInfo[0]);
+            newMember.setMemberRank(Integer.parseInt(memberInfo[1]));
+            newMember.setMemberCredits(Integer.parseInt(memberInfo[2]));
+            newMember.setMemberDiscordId(Long.parseLong(memberInfo[3]));
+            newMember.setMemberDateJoined(LocalDate.parse(memberInfo[4], SithClanPluginConstants.DATE_FORMATTER));
+            newMember.setMemberAltName(memberInfo[5].isBlank() ? null : memberInfo[5]);
+            newMember.setMemberDatePromoted(memberInfo[6].isBlank() ? null
+                    : LocalDate.parse(memberInfo[6], SithClanPluginConstants.DATE_FORMATTER));
+
+            // add member to roster
+            newRoster.put(newMember.getMemberName().toLowerCase(), newMember);
+
+        }
+        return newRoster;
+    }
+
+    /**
+     * Deserializes JSON string to HashMap roster
+     * 
+     * @param jsonRoster JSON String of member roster
+     * @return HashMap<String, SithClanMember> deserialized member roster
+     */
+    private HashMap<String, SithClanMember> deserializeRoster(String jsonRoster) {
+        // convert roster to JSON
+        Gson gson = new Gson();
+        RosterResponse response = gson.fromJson(jsonRoster, RosterResponse.class);
+        ZonedDateTime utcTime = ZonedDateTime.parse(response.date);
+        this.dateRosterPosted = utcTime.withZoneSameInstant(ZoneId.systemDefault());
+        HashMap<String, SithClanMember> roster = new HashMap<>();
+        for (SithClanMember member : response.roster) {
+            roster.put(member.getMemberName().toLowerCase(), member);
+        }
+        return roster;
     }
 
     /**
      * Searches for member in roster by name
-     * TODO: what if member does not exist
      * 
      * @param name String name of member to search for
      * @return SithClanMember member searched for
