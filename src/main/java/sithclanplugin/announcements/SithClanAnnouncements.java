7d69ddf237781c72f7c29463d0c72fe2552bdd1b
Tyler <psenk168@gmail.com>
2026-03-27 22:11:31 -0500

    added getter and function stubs

--- src/main/java/sithclanplugin/announcements/SithClanAnnouncements.java
+++ src/main/java/sithclanplugin/announcements/SithClanAnnouncements.java
@@ -1,16 +1,24 @@
 package sithclanplugin.announcements;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
 import com.google.inject.Singleton;
 
+import lombok.Getter;
+
 @Singleton
 public class SithClanAnnouncements {
 
+    @Getter
     private ArrayList<SithClanAnnouncement> announcements;
+
+    private ZonedDateTime announcementsUpdated;
 
     public SithClanAnnouncements() {
         announcements = new ArrayList<>();
+        announcementsUpdated = ZonedDateTime.now().withZoneSameInstant(ZoneId.systemDefault());
     }
 
     /**
@@ -20,6 +28,33 @@ public class SithClanAnnouncements {
      */
     public void loadStartupAnnouncements(ArrayList<SithClanAnnouncement> announcement) {
         this.announcements = announcement;
+        this.announcementsUpdated = ZonedDateTime.now().withZoneSameInstant(ZoneId.systemDefault());
     }
 
+    public int parseAnnouncementForPost(String text) {
+        return 0;
+    }
+
+    /**
+     * TODO: FUNCTIONALITY
+     * TODO: JAVADOC
+     * 
+     * @param id
+     * @param announcement
+     * @return
+     */
+    public int parseAnnouncementForEdit(int id, String announcement) {
+        return 0;
+    }
+
+    /**
+     * TODO: FUNCTIONALITY
+     * TODO: JAVADOC
+     * 
+     * @param id
+     * @return
+     */
+    public int deleteAnnouncement(int id) {
+        return 0;
+    }
 }
