65eb8ce6b71b15e48466fce492b5428e067f52a6
psenk <psenk168@gmail.com>
2026-06-26 18:12:09 -0500

    add clan attendance import config option create configsections

--- src/main/java/sithclanplugin/SithClanConfig.java
+++ src/main/java/sithclanplugin/SithClanConfig.java
@@ -28,37 +28,51 @@ package sithclanplugin;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 import net.runelite.client.config.Range;
 
 @ConfigGroup("sithclanplugin")
 public interface SithClanConfig extends Config
 {
-	@ConfigItem(keyName = "eventNotifications", name = "Event Alerts", description = "Enables or disables notifications for upcoming clan events.", position = 1)
+	@ConfigSection(name = "Clan Events", description = "Clan event configuration options.", position = 0)
+	String eventsSection = "eventsSection";
+
+	@ConfigItem(keyName = "eventNotifications", name = "Event Alerts", description = "Enables or disables notifications for upcoming clan events.", section = "eventsSection", position = 0)
 	default boolean eventNotifications()
 	{
 		return false;
 	}
 
 	@Range(min = 1, max = 60)
-	@ConfigItem(keyName = "eventAlertTimeBuffer", name = "Alert Time Buffer", description = "Sets amount of time in minutes before event notification occurs.", position = 2)
+	@ConfigItem(keyName = "eventAlertTimeBuffer", name = "Alert Time Buffer", description = "Sets amount of time in minutes before event notification occurs.", section = "eventsSection", position = 1)
 	default int eventAlertTimeBuffer()
 	{
 		return 15;
 	}
 
-	@ConfigItem(keyName = "memberLookupMenu", name = "Clan Member Lookup", description = "Enables option to lookup clan members in right-click menus.", position = 3)
-	default boolean memberLookupMenu()
+	@ConfigItem(keyName = "attendanceImport", name = "Import Clan Attendance", description = "Enables the option to import event logs directly from the Clan Event Attendance plugin. "
+			+ "WARNING: The 'File Save -> Save Locally' setting must be enabled on the Clan Event Attendance plugin for this to work.", section = "eventsSection", position = 2)
+	default boolean attendanceImport()
 	{
 		return false;
 	}
 
-	@ConfigItem(keyName = "eventLogWebhook", name = "Discord Event Log Webhook URL", description = "Discord webhook URL to post event logs", position = 4)
+	@ConfigItem(keyName = "eventLogWebhook", name = "Discord Event Log Webhook URL", description = "Discord webhook URL to post event logs", section = "eventsSection", position = 3)
 	default String eventLogWebhook()
 	{
 		return "".trim();
 	}
 
-	@ConfigItem(keyName = "senateApiKey", name = "Senate API Key", description = "API Key for access to Senate member plugin options", position = 5)
+	@ConfigSection(name = "Miscellaneous", description = "Miscellaneous configuration options.", position = 1)
+	String miscSection = "miscSection";
+
+	@ConfigItem(keyName = "memberLookupMenu", name = "Clan Member Lookup", description = "Enables option to lookup clan members in right-click menus.", section = "miscSection", position = 0)
+	default boolean memberLookupMenu()
+	{
+		return false;
+	}
+
+	@ConfigItem(keyName = "senateApiKey", name = "Senate API Key", description = "API Key for access to Senate member plugin options", section = "miscSection", position = 999)
 	default String senateApiKey()
 	{
 		return "".trim();
